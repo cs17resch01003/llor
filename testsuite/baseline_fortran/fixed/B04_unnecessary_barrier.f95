52a4a928c8bda3049cadd49067227f1e546cd052
Gautam Muduganti <gmuduganti@spglobal.com>
2024-03-25 04:57:49 +0530

    Fixing test cases
    Fixing test cases.

---
+++ testsuite/baseline_fortran/fixed/B04_unnecessary_barrier.f95
@@ -0,0 +1,22 @@
+!; Pass
+!; Remove the barrier at line number 20.
+
+program B04_unnecessary_barrier
+    use omp_lib
+
+    implicit none
+    integer :: i, id, temp, threads = 10
+    integer :: data(11)
+
+    call omp_set_num_threads(threads)
+    do i = 1, threads+1
+        data(i) = i
+    end do
+
+    !$omp parallel private(id,temp)
+        id = omp_get_thread_num()
+
+        temp = id+10
+		data(id) = temp
+    !$omp end parallel
+end program
